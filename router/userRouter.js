f4d6f4dc4086052ba6261e09ebc3d8487fc22f10
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-02-23 15:52:18 -0800

    added comment to codes

--- router/userRouter.js
+++ router/userRouter.js
@@ -10,11 +10,22 @@ import {
 
 const router = express.Router();
 
+// get all user 
 router.get("/",getAllUser);
+
+//get single user
 router.get("/:id", getSingleUser);
+
+// create new user
 router.post("/", createNewUser);
+
+// update user
 router.put("/:id", updateUser);
+
+//delete all user
 router.delete("/", deleteUsers);
+
+// delete single user
 router.delete("/:id", deleteSingleUser);
 
 export default router;
